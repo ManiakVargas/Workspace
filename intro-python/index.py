7a9264f27e631ee0ce44fde778e4580973a14fc5
ManiakVargas <102061299+ManiakVargas@users.noreply.github.com>
2022-04-29 04:52:52 -0500

    Archivos de los capitulos del curso

---
+++ intro-python/index.py
@@ -0,0 +1,41 @@
+# Acá va un comentario
+if 3 > 5:
+  print('esto no se va a imprimir')
+#if 5 > 3: #Aca va otro comentario
+   # print('5 es mayor a 3')
+
+
+x = 5
+y = 'Chanchito feliz'
+
+#print (x, y)
+email = 'chanchito@feliz.com'
+
+print (email)
+
+mivar = 'Chancho'
+
+a, b, c = 'lala', 'lele', 'lili'
+
+#print (a, b, c)
+
+valor1 = valor2 = valor3= 'chanchito feliz'
+
+#print(valor1, valor2, valor3)
+
+inicio = 'hola '
+final = 'Mundo'
+
+print(inicio + final)
+
+lista = [3, 2, 3]
+lista2 = lista. copy()
+lista. append(4)
+
+#print (lista, lista2)
+#print (lista, lista2. count(3))
+#print(len(lista), len(lista2))
+
+largolista = len(lista)
+largolista2 = len(lista2)
+print(largolista, largolista2)
